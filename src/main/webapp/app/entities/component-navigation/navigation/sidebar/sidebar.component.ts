b99b699498a86d92b659d9387e90503d732b8bcb
monjur <monjurmorshed793@gmail.com>
2021-10-27 01:11:59 +0600

    navigation grouped between parent-child

--- src/main/webapp/app/entities/component-navigation/navigation/sidebar/sidebar.component.ts
+++ src/main/webapp/app/entities/component-navigation/navigation/sidebar/sidebar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ComponentNavigationService } from '../../service/component-navigation.service';
 import { ComponentNavigation, IComponentNavigation } from '../../component-navigation.model';
+import { MenuItem } from 'primeng/api';
+import { MenuItemContent } from 'primeng/menu';
 
 @Component({
   selector: 'jhi-sidebar',
@@ -9,12 +11,30 @@ import { ComponentNavigation, IComponentNavigation } from '../../component-navig
 })
 export class SidebarComponent implements OnInit {
   componentNavigations: IComponentNavigation[] = [];
+  items: MenuItem[] = [];
 
   constructor(private componentNavigationService: ComponentNavigationService) {}
 
   ngOnInit(): void {
     this.componentNavigationService.query().subscribe(res => {
       this.componentNavigations = res.body!;
+      const parents: IComponentNavigation[] = [];
+      const parentMapChild = new Map();
+      this.componentNavigations.forEach(n => {
+        if (n.location === '#') {
+          parents.push(n);
+        } else {
+          if (parentMapChild.has(n.parent)) {
+            const child = parentMapChild.get(n.parent);
+            child.push(n);
+            parentMapChild.set(n.parent, child);
+          } else {
+            const child: IComponentNavigation[] = [];
+            child.push(n);
+            parentMapChild.set(n.parent, child);
+          }
+        }
+      });
     });
   }
 }
